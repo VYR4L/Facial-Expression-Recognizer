5c9f201e59a879743ee526fd759868a9fd986128
VYR4L <fkz.zanatt@gmail.com>
2024-09-17 19:09:37 -0300

    Contagem das amostras

--- training.py
+++ training.py
@@ -10,42 +10,37 @@ from torch.utils.data import Dataset, DataLoader
 
 
 ROOT_DIR = Path(__file__).parent
-TRAIN_IMAGES = ROOT_DIR / 'dataset' / 'images'
-
+DATA_SET = ROOT_DIR / 'dataset' / 'images'
+TRAIN_IMAGES = ROOT_DIR / 'dataset' / 'images' / 'train'
+VALIDATION_IMAGES = ROOT_DIR / 'dataset' / 'images' / 'validation'
 
 output_type = {}
 count_type = [0 for i in range(7)]
 
-for index, val in enumerate(glob.glob(f'{TRAIN_IMAGES}/train/*')):
-    output_type[val.split('/')[6]] = index
-
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+for index, val in enumerate(glob.glob(str(TRAIN_IMAGES / '*'))):
+    output_type[Path(val).parts[-1]] = index
 
 
 class Data(Dataset):
-    def __init__(self, is_train = True):
+    def __init__(self, is_train=True):
         self.is_train = is_train
-        data_type = ""
-        if not is_train:
-            data_type = 'test'
-        else:
-            data_type = 'train'
+        data_type = 'train' if is_train else 'validation'
         
-        self.path_file = glob.glob(f'{TRAIN_IMAGES}/images/{0}/*/*')
+        self.path_file = glob.glob(str(DATA_SET / data_type / '*' / '*'))
 
         if is_train:
             temp_data = []
             for i in range(9):
-                self.path_file += glob.glob(f'{TRAIN_IMAGES}/images/{i}/disgust/*')
+                self.path_file += glob.glob(str(DATA_SET / data_type / 'disgust' / '*'))
             
             for type in output_type:
                 counter = 0
                 for path in self.path_file:
-                    for i in range(2):
-                        if path.split('/')[6] == type and counter < 7000:
-                            counter += 1
-                            temp_data.append(path)
-                            count_type[output_type[type]] += 1
+                    path_parts = Path(path).parts
+                    if len(path_parts) > 6 and path_parts[-2] == type and counter < 7000:
+                        counter += 1
+                        temp_data.append(path)
+                        count_type[output_type[type]] += 1
             self.path_file = temp_data
 
         self.transform = ToTensor()
@@ -57,27 +52,25 @@ class Data(Dataset):
                                 RandomVerticalFlip(),
         )
 
-
     def __len__(self):
         return len(self.path_file)
-    
 
     def __getitem__(self, idx):
         image_path = self.path_file[idx]
         image = Image.open(image_path)
-        label = output_type[image_path.split('/')[6]]
+        label = output_type[Path(image_path).parts[-2]]
         image = self.transform(image)
         if self.is_train:
             image = self.data(image)
-        return image, label
-    
-training = Data(is_train=True)
-training.to(device)
+        
+        return (image, label)
 
-for key, val in enumerate(output_type):
-    print(f'{val}: {count_type[key]}')
+
+training = Data(is_train=True)
+
+for key, val in output_type.items():
+    print(f'{key}: {count_type[val]}')
 
 validation_data = Data(is_train=False)
 train_dataloader = DataLoader(training, batch_size=64, shuffle=True)
 validation_dataloader = DataLoader(validation_data, batch_size=64, shuffle=True)
-
