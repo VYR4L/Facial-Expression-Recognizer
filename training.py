4ffa45ceb12c35149fc74ab4d8b395b25d84feed
VYR4L <fkz.zanatt@gmail.com>
2024-09-17 19:40:53 -0300

    Código com o treinamento

--- training.py
+++ training.py
@@ -37,7 +37,7 @@ class Data(Dataset):
                 counter = 0
                 for path in self.path_file:
                     path_parts = Path(path).parts
-                    if len(path_parts) > 6 and path_parts[-2] == type and counter < 7000:
+                    if len(path_parts) > 6 and path_parts[-2] == type:
                         counter += 1
                         temp_data.append(path)
                         count_type[output_type[type]] += 1
@@ -74,3 +74,96 @@ for key, val in output_type.items():
 validation_data = Data(is_train=False)
 train_dataloader = DataLoader(training, batch_size=64, shuffle=True)
 validation_dataloader = DataLoader(validation_data, batch_size=64, shuffle=True)
+
+
+class ResidualCNN(nn.Module):
+    def __init__(self, input_channels, output_channels):
+        super().__init__()
+        self.block = nn.Sequential(
+            nn.Conv2d(input_channels, output_channels, 3, padding=1),
+            nn.BatchNorm2d(output_channels),
+            nn.ReLU(),
+            nn.Dropout(p=0.25),
+            nn.Conv2d(output_channels, output_channels, 3, padding=1),
+            nn.BatchNorm2d(output_channels),
+        )
+
+        self.resize = nn.Conv2d(input_channels, output_channels, 1)
+
+    def forward(self, x):
+        return nn.Dropout(p=0.25)(nn.ReLU()(self.block(x) + self.resize(x)))
+    
+
+class TrainModule(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.neutral_network = nn.Sequential(
+            nn.Conv2d(1, 64, 5, padding=2),
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+            nn.MaxPool2d(3, stride=2, padding=1),
+            nn.Dropout(p=0.25),
+
+            ResidualCNN(64, 128),
+            ResidualCNN(128, 256),
+            ResidualCNN(256, 512),
+            ResidualCNN(512, 512),
+
+            nn.AdaptiveAvgPool2d((1, 1)),
+            nn.Flatten(),
+            nn.Linear(512, 7),
+            nn.Softmax(dim=1),
+            )
+        
+    def forward(self, x):
+        return self.neutral_network(x)
+    
+
+learning_rate = 0.0256
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+model = TrainModule().to(device)
+loss_fn = nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+
+
+def train(data_loader, model, loss_function, optimizer):
+    size = len(data_loader.dataset)
+    model.train()
+    for batch, (X, y) in enumerate(data_loader):
+        predict = model(X.to(device))
+        loss = loss_function(predict, y.to(device))
+
+        loss.backward()
+        optimizer.step()
+        optimizer.zero_grad()
+
+        if batch % 100 == 0:
+            loss, current = loss.item(), (batch + 1) * len(X)
+            print(f'loss: {loss:>7f}  [{current:>5d}/{size:>5d}]')
+
+
+def test(data_loader, model, loss_function):
+    size = len(data_loader.dataset)
+    model.eval()
+    size = len(data_loader.dataset)
+    num_batches = len(data_loader)
+    test_loss, correct = 0, 0
+
+    with torch.no_grad():
+        for X, y in data_loader:
+            predict = model(X.to(device))
+            test_loss += loss_function(predict, y.to(device)).item()
+            correct += (predict.argmax(1) == y.to(device)).type(torch.float).sum().item()
+
+        test_loss /= num_batches
+        correct /= size
+        print(f'Test Error: \n Accuracy: {(100 * correct):>0.1f}%, Avg loss: {test_loss:>8f} \n')
+
+
+epochs = 100
+for t in range(epochs):
+    print(f'Epoch {t + 1}\n-------------------------------')
+    train(train_dataloader, model, loss_fn, optimizer)
+    test(validation_dataloader, model, loss_fn)
